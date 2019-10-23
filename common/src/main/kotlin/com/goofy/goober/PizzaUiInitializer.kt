b998875b5d538393bd93f1c4832a88ef9b91a5f0
drinkthestars <2978958+drinkthestars@users.noreply.github.com>
2019-10-23 13:17:01 -0700

    Updated project settings for AS 4.0 Canary + compose dev2

--- common/src/main/kotlin/com/goofy/goober/PizzaUiInitializer.kt
+++ common/src/main/kotlin/com/goofy/goober/PizzaUiInitializer.kt
@@ -1,18 +1,20 @@
 package com.goofy.goober
 
 import com.goofy.goober.model.PizzaAction
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.withContext
 
 class PizzaUiInitializer {
 
     operator fun invoke(block: PizzaUiInitializer.() -> Unit) = block()
 
-    suspend fun PizzaUi.initialize() = withContext(Dispatchers.IO) {
-        delay(2_000)
-        withContext(Dispatchers.Main.immediate) {
-            initialize(action = PizzaAction.ShowWelcomeScreen)
-        }
+    // Commenting out since this isn't working with the stdlib 1.3.60-eap-25
+//    suspend fun PizzaUi.initialize() = withContext(Dispatchers.IO) {
+//        delay(2_000)
+//        withContext(Dispatchers.Main.immediate) {
+//            initialize(action = PizzaAction.ShowWelcomeScreen)
+//        }
+//    }
+
+    fun PizzaUi.initialize() {
+        initialize(action = PizzaAction.ShowWelcomeScreen)
     }
 }
